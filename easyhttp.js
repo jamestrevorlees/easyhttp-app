1e624695a74a60554f008184281776cf40437eb7
James Lees <jamestrevorlees@gmail.com>
2020-08-31 05:33:27 +0200

    create get method and setup error handling

--- easyhttp.js
+++ easyhttp.js
@@ -4,12 +4,15 @@ function easyHTTP() {
 
 // Make an HTTP GET Request
 
-easyHTTP.prototype.get = function (url) {
+easyHTTP.prototype.get = function (url, callback) {
   this.http.open("GET", url, true);
 
+  let self = this;
   this.http.onload = function () {
-    if (this.http.status === 200) {
-      console.log(this.http.responseText);
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: " + self.http.status);
     }
   };
 
